537a8c3cfbfd36257470f8a17729ef8d0991e582
Hossmi Nava <tisdalesh2o@gmail.com>
2018-04-20 20:54:27 -0500

    clase de condicionales

--- condicionales.js
+++ condicionales.js
@@ -3,24 +3,21 @@ let edad=7;
 
 if(edad>=18){
     console.log('ya es mayor de edad');
-    if(edad>=40 & edad<=60) {
+    if(edad>=40 & edad<=60){
         console.log('Podira ser tu jefe');
     }  
-    if(edad>60 & edad <=90)
+    else if(edad>60 & edad <=90){
     console.log('podria ser tu abuelo');
     }
-}
 
 else{
     console.log('Ya es una momia'); 
+  }
 }
-
-else {
-    console.log('esta morro');
-
+else{
+    console.log('esta  morro');
     if(edad>=15 & edad<18){
     console.log('esta en la prepa');
-
     }
  if(edad>=12 && edad<15){
      console.log('esta en la secundaria');
