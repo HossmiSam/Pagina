d34cdb0624c38af6b8ad75bf39a7f0551af12722
Hossmi Nava <tisdalesh2o@gmail.com>
2018-04-24 20:00:21 -0500

    primera clase JS

---
+++ json.js
@@ -0,0 +1,32 @@
+
+let cinta_blanda = [
+    {
+        'nombre':'Omar'
+        'edad':24
+    },
+    {
+        'nombre' : 'Francisco'
+        'edad':39
+    },
+    {
+        'nombre' : 'Hossmi'
+        'edad':25
+    
+    }
+]
+    let sujeto={
+        'nombre': 'Hossmi'
+        'edad':25
+    }
+        //console.long (cinta_blanca);
+
+        console.log(sujeto);
+    console.log('Esta persona se llama '+cinta_blanda[1].nombre+
+                        ' y tiene '+ cinta_blanda[i].edad+' años');
+                   
+    for(i=0;i<cinta_blanda.length;i++)
+    console.log('Esta persona se llama '+cinta_blanda[1].nombre+
+                        ' y tiene '+ cinta_blanda[i].edad+' años');          
+
+        //cinta_blanca.push(sujeto)
+        //console.log(cinta_blanca);
