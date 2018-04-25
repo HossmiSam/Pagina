db495907409d9e1447b6d4dae3ee34fbc7fe98e2
Hossmi Nava <tisdalesh2o@gmail.com>
2018-04-24 21:45:41 -0500

    Codigo Js de practica

---
+++ html-js.js
@@ -0,0 +1,30 @@
+
+
+let cinta_blanda = [
+{
+'nombre':'Omar',
+'edad':24
+},
+{
+'nombre' : 'Francisco',
+'edad':39
+},
+{
+'nombre' : 'Nora',
+'edad':25
+},
+{
+'nombre' : 'Hossmi',
+'edad':25
+}
+]
+
+for(i=0;i<cinta_blanca.length; i++){
+
+
+//Creamos elementos de jquery, lo mismo de arriba pero con los objetos de jquery
+
+let h1=$('<h1>'+'Esta persona se llama '+cinta_blanca[i].nombre+' y tiene '+cinta_blanca[i].edad' años'+'<h1>'
+$('#elBody').append(h1);
+
+}
