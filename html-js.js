abdab71142b32d80078cbce0028b4564be55169a
Hossmi Nava <tisdalesh2o@gmail.com>
2018-04-24 21:49:48 -0500

    Codigo con Jquery

--- html-js.js
+++ html-js.js
@@ -20,11 +20,8 @@ let cinta_blanda = [
 ]
 
 for(i=0;i<cinta_blanca.length; i++){
-
-
 //Creamos elementos de jquery, lo mismo de arriba pero con los objetos de jquery
-
-let h1=$('<h1>'+'Esta persona se llama '+cinta_blanca[i].nombre+' y tiene '+cinta_blanca[i].edad' años'+'<h1>'
+let h1=$('<h1>'+'Esta persona se llama '+cinta_blanca[i].nombre+' y tiene '+cinta_blanca[i].edad+' años'+'</h1>'); 
 $('#elBody').append(h1);
 
 }
