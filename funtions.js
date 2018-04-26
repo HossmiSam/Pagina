04bef7b943dc6c62a12bbcd86b2661233770aa19
Hossmi Nava <tisdalesh2o@gmail.com>
2018-04-25 20:18:13 -0500

    Clase de funciones

---
+++ funtions.js
@@ -0,0 +1,7 @@
+function elBotonazo(mensaje){
+    alert(mensaje+","+mensaje.length);
+}
+function elDialogo(){
+    let elNombre=document.getElementById('elNombre').value;
+    elBotonazo('Que onda '+elNombre+', Como andas?');
+}
